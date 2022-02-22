9f8c12509ddc0e37ea98fe30519816ea74af268f
Hongsuk <hongsuk.ryu@silcraodsoft.com>
2022-02-22 17:45:12 +0900

    package file updated

---
+++ src/components/travelHistory/TravelHistory.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const dummy_data = {
+  userId: 'abcd123',
+  images: '',
+};
+
+/**
+ * dummy data
+ *
+ * @returns
+ */
+function TravelHistory() {
+  return <div>TravelHistory</div>;
+}
+
+export default TravelHistory;
