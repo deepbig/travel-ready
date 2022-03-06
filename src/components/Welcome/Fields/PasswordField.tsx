0f23e97368daf0fcccc46e6ed302b803a8f04e3a
kylaokobah <89166788+kylaokobah@users.noreply.github.com>
2022-03-06 10:14:29 -0500

    login, register, forgot v12

--- src/components/Welcome/Fields/PasswordField.tsx
+++ src/components/Welcome/Fields/PasswordField.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from 'react';
 import TextField from "@material-ui/core/TextField";
 import FormControl from "@material-ui/core/FormControl";
 import FormHelperText from "@material-ui/core/FormHelperText";
